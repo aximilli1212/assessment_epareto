51b6070f259123be2afc251b942cc67eca73481b
Aximilli1212 <aximill1212@gmail.com>
2020-05-09 10:20:50 +0000

    fetch all books added

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//ALL BOOKS ROUTES
+Route::get('/books','BooksController@index');
+
